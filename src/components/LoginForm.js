afd7c87b283bd8476616bdf6eb1b5735858729bf
FridaSjoholm <frida@fridaesign.se>
2017-04-20 10:14:30 -0700

    Authenticates user with firebase, if not user, creates user or gives errormessage

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' }
+  state = { email: '', password: '', error: '' }
+
+  onButtonPress() {
+    console.log('button');
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed' });
+          });
+      });
+  }
 
   render() {
+    console.log(this.state);
     return (
-      <View>
       <Card>
         <CardSection>
           <Input
@@ -26,15 +39,25 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
       </Card>
-      </View>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
