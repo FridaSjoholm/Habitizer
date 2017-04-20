c2fbb2c61bba8c893d91de7bb75d3a51ccc3e263
FridaSjoholm <frida@fridaesign.se>
2017-04-19 17:28:39 -0700

    Sets up firebase for auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
  class App extends Component {
+   componentWillMount() {
+     firebase.initializeApp({
+       apiKey: 'AIzaSyBd1YTJmTNw6Og1HwurJ7s6fvOq8NaIU8o',
+       authDomain: 'habitizer-5aa0d.firebaseapp.com',
+       databaseURL: 'https://habitizer-5aa0d.firebaseio.com',
+       projectId: 'habitizer-5aa0d',
+       storageBucket: 'habitizer-5aa0d.appspot.com',
+       messagingSenderId: '495005308695'
+     });
+   }
+
    render() {
      return (
        <View>
