1a58b3ed50cd6ba64579e6c0b53cd1e35cc59875
FridaSjoholm <frida@fridaesign.se>
2017-04-23 16:51:26 -0700

    Reactors to make app about habits instead of employees

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
-import EmployeeList from './components/EmployeeList';
+import HabitList from './components/HabitList';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="login" component={LoginForm} title="Please Login" />
-      <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Scene>
+
+      <Scene key="main">
+        <Scene key="habitList"
+        component={HabitList}
+        title="Habits" />
+      </Scene>
     </Router>
   );
 };
---
+++ src/components/HabitList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class HabitList extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Habit A </Text>
+        <Text> Habit B </Text>
+        <Text> Habit C </Text>
+        <Text> Habit D </Text>
+        <Text> Habit E </Text>
+        <Text> Habit F </Text>
+        <Text> Habit G </Text>
+      </View>
+    );
+  }
+}
+
+export default HabitList;
