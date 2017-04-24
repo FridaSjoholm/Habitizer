78940bf17a507cbf1057cc286fbb9427e8919fc8
FridaSjoholm <frida@fridaesign.se>
2017-04-24 09:41:54 -0700

    Adds form outline

--- src/components/HabitsCreate.js
+++ src/components/HabitsCreate.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class HabitCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Habit Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Habit"
+            placeholder="Vacuum"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Description"
+            placeholder="All tha floors"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Text>Daypicker</Text>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
