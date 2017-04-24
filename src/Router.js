dac8545529870f318ff1da6fd6009b68f956ff4b
FridaSjoholm <frida@fridaesign.se>
2017-04-23 17:05:08 -0700

    Adds link to form and component for form

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import HabitList from './components/HabitList';
+import HabitsCreate from './components/HabitsCreate';
 
 const RouterComponent = () => {
   return (
@@ -11,9 +12,20 @@ const RouterComponent = () => {
       </Scene>
 
       <Scene key="main">
-        <Scene key="habitList"
-        component={HabitList}
-        title="Habits" />
+        <Scene
+          key="habitList"
+          component={HabitList}
+          title="Habits"
+          rightTitle="Add"
+          onRight={() => Actions.habitsCreate()}
+          initial
+        />
+
+        <Scene
+          key="habitsCreate"
+          component={HabitsCreate}
+          title="Create Habit"
+        />
       </Scene>
     </Router>
   );
---
+++ src/components/HabitsCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class HabitCreate extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Habit Form</Text>
+      </View>
+    );
+  }
+}
+
+export default HabitCreate;
